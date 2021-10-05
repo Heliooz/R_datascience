ed1b4ec4c61c6b0f968ab261d55e696e0670a5e2
Clément <clem.maurer@hotmail.fr>
2021-10-05 16:46:15 +0200

    End of 5/10

--- Projet/main.R
+++ Projet/main.R
@@ -1,13 +1,19 @@
 rm(list = ls())
 
 library(tidyverse)
+library(tmap)
+library(tmaptools)
+library(sf)
+library(spData)
+library(spDataLarge)
 
 data <- read.csv("Listings.csv")
-
 data <- as.data.frame(data)
+
 paris <- filter(data, city=="Paris", price<= 500)
 
-ggplot(paris, aes(x = price)) + geom_histogram()
+
+
 
 
        
