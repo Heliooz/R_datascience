3c8232b5e67792f8acc50bfa90739fa6a5873cda
Dem's <jeremy.demay1@outlook.fr>
2021-11-12 23:22:36 +0100

    main

--- Projet/main_r.R
+++ Projet/main_r.R
@@ -1,22 +1,11 @@
-rm(list = ls())
 
-library(tidyverse)
-library(sf)
-library(rnaturalearth)
-library(rnaturalearthdata)
+##Appel des fichier code
+source("global.R")
+source("ui.R")
+source("server.R")
 
-data <- as.data.frame(read.csv("Listings.csv"))
-paris <- filter(data, city=="Paris")
-
-ggplot(paris, aes(x=longitude, y=latitude, color=neighbourhood)) + geom_point()
-
-map_arrondissements <- as.data.frame(read_delim("arrondissements.csv", delim = ';'))
-map_arrondissements <- map_arrondissements['geom']
-
-ggplot()
-
-test <- geojsonsf::geojson_sf(map_arrondissements)
-
+##Demarrage de l'application
+shinyApp(ui, server)
 
 
 
